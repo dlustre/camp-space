94bedd0193d01725cba0eda1c576cbc6b6c4b23e
Dennis Lustre <dennis.lustre@gmail.com>
2024-01-25 02:31:57 -0800

    re-add output file name

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   basePath: process.env.NODE_ENV === 'development' ? undefined : "/camp-space",
   images: {
     unoptimized: true
