af5ba623eec6637cab5adc96dbd85f8b5963ac16
Dennis Lustre <dennis.lustre@gmail.com>
2024-02-13 00:36:56 -0800

    try with assetprefix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 const withMDX = require('@next/mdx')()
 
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === 'development' ? undefined : "/camp-space",
+  basePath: isProd ? "/camp-space" : undefined,
+  assetPrefix: isProd ? "/camp-space/" : undefined,
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   images: {
     unoptimized: true
